d01d523bc8e72e9ff175b9358961912cf6e7cc5b
zhaokai <battleboy@qq.com>
2025-04-24 16:13:12 +0800

    add 10004

--- src/utils/request.js
+++ src/utils/request.js
@@ -81,7 +81,7 @@ service.interceptors.response.use(res => {
     if (res.request.responseType ===  'blob' || res.request.responseType ===  'arraybuffer') {
       return res.data
     }
-    if (code === 401) {
+    if (code === 401 || code === 10004) {
       if (!isRelogin.show) {
         isRelogin.show = true;
         ElMessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
