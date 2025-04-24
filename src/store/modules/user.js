987165162eef9150563700178a8bec9ac76e3793
zhaokai <battleboy@qq.com>
2025-04-24 16:32:01 +0800

    commit

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -73,7 +73,16 @@ const useUserStore = defineStore(
             reject(error)
           })
         })
-      }
+      },
+      logoutWithoutRequest() {
+        return new Promise((resolve, reject) => {
+          this.token = ''
+          this.roles = []
+          this.permissions = []
+          removeToken()
+          resolve()
+        })
+      },
     }
   })
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -86,7 +86,9 @@ service.interceptors.response.use(res => {
         isRelogin.show = true;
         ElMessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
           isRelogin.show = false;
-          location.href = '/index';
+          useUserStore().logoutWithoutRequest().then(() => {
+            location.href = '/index';
+          })
       }).catch(() => {
         isRelogin.show = false;
       });
